88433dfae268976417d99168001ec453375bd305
hossein1387 <m.h.askari.hemmat@gmail.com>
2022-01-23 18:44:43 -0500

    placing vpopcnt in alu section of the enums

--- hardware/include/ara_pkg.sv
+++ hardware/include/ara_pkg.sv
@@ -155,9 +155,7 @@ package ara_pkg;
     // Load instructions
     VLE, VLSE, VLXE,
     // Store instructions
-    VSE, VSSE, VSXE,
-    // Popcount instruction
-    VPOPCNT
+    VSE, VSSE, VSXE
   } ara_op_e;
 
   // Return true if op is a load operation
