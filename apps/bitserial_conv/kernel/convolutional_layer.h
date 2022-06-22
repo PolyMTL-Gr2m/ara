d09256f203c9407a3008510883eeafa9860b5313
hossein1387 <m.h.askari.hemmat@gmail.com>
2022-06-22 18:41:12 -0400

    renaming convolutional to conv2d

--- apps/bitserial_conv/kernel/convolutional_layer.h
+++
@@ -1,9 +0,0 @@
-#ifndef CONVOLUTIONAL_LAYER_H
-#define CONVOLUTIONAL_LAYER_H
-
-#include "layer.h"
-
-typedef layer convolutional_layer;
-convolutional_layer make_convolutional_layer(int batch, int h, int w, int c, int n, int groups, int size, int stride, int padding, int batch_normalize, int binary, int xnor, int adam);
-
-#endif
