2968dbecf8cfb901977c61583e84e20dff84ddb9
M-Ijaz-10x <muhammad.ijaz@10xengineers.ai>
2022-09-28 15:44:28 +0500

    [apps] update vssra, vssrl, vnclip, vnclipu and vsmul tests for rounding mode

--- apps/riscv-tests/isa/rv64uv/vnclip.c
+++ apps/riscv-tests/isa/rv64uv/vnclip.c
@@ -10,6 +10,7 @@ void TEST_CASE1() {
   VSET(4, e8, m1);
   VLOAD_16(v2, 800, 65535, -50, 25);
   VLOAD_8(v4, 7, 7, 7, 7);
+  __asm__ volatile("csrw vxrm, 2");
   __asm__ volatile("vnclip.wv v1, v2, v4");
   VCMP_I8(1, v1, 6, 0xff, 0xff, 0);
 }
@@ -20,6 +21,7 @@ void TEST_CASE2() {
   VLOAD_8(v4, 7, 7, 7, 7);
   VLOAD_8(v0, 0x5, 0, 0, 0);
   VCLEAR(v1);
+  __asm__ volatile("csrw vxrm, 2");
   __asm__ volatile("vnclip.wv v1, v2, v4, v0.t");
   VCMP_I8(2, v1, 6, 0, 0xff, 0);
 }
@@ -28,6 +30,7 @@ void TEST_CASE3() {
   VSET(4, e8, m1);
   VLOAD_16(v2, 800, 65535, -50, 25);
   int8_t scalar = 7;
+  __asm__ volatile("csrw vxrm, 2");
   __asm__ volatile("vnclip.wx v1, v2, %[A]" ::[A] "r"(scalar));
   VCMP_I8(3, v1, 6, 0xff, 0xff, 0);
 }
@@ -38,6 +41,7 @@ void TEST_CASE4() {
   int8_t scalar = 7;
   VLOAD_8(v0, 0x5, 0, 0);
   VCLEAR(v1);
+  __asm__ volatile("csrw vxrm, 2");
   __asm__ volatile("vnclip.wx v1, v2, %[A], v0.t" ::[A] "r"(scalar));
   VCMP_I8(4, v1, 6, 0, 0xff, 0);
 }
@@ -45,6 +49,7 @@ void TEST_CASE4() {
 void TEST_CASE5() {
   VSET(4, e8, m1);
   VLOAD_16(v2, 800, 65535, -50, 25);
+  __asm__ volatile("csrw vxrm, 2");
   __asm__ volatile("vnclip.wi v1, v2, 7");
   VCMP_I8(5, v1, 6, 0xff, 0xff, 0);
 }
@@ -54,6 +59,7 @@ void TEST_CASE6() {
   VLOAD_16(v2, 800, 65535, -50, 25);
   VLOAD_8(v0, 0x5, 0, 0, 0);
   VCLEAR(v1);
+  __asm__ volatile("csrw vxrm, 2");
   __asm__ volatile("vnclip.wi v1, v2, 7, v0.t");
   VCMP_I8(6, v1, 6, 0, 0xff, 0);
 }
