1e3ca93d30b11b6fd78a9fc000c526fa0dff5744
Yoan Fournier <fournier@srvgrm11.grm.polymtl.ca>
2023-12-06 13:27:14 -0500

    Fixing issue with bitpack/util.h

--- apps/bitpack/util.h
+++ apps/bitpack/util.h
@@ -25,10 +25,7 @@
 
 void  __printb(void *value, size_t size);
 void print_matrix(uint64_t *mat, int num_rows, int num_columns, int bin_print_format);
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> 855f49a (python model for bitserial convolution)
+
 const uint64_t vshift[64] = {  0,  1,  2,  3,  4,  5,  6,  7, 
                                8,  9, 10, 11, 12, 13, 14, 15,
                               16, 17, 18, 19, 20, 21, 22, 23,
